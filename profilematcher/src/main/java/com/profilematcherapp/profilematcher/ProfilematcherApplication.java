28fdf0ba3d45926e555eda67f43ee4f0eb9bf634
csgtdev04 <62195113+csgtdev04@users.noreply.github.com>
2021-10-30 15:37:01 -0500

    first commit

---
+++ profilematcher/src/main/java/com/profilematcherapp/profilematcher/ProfilematcherApplication.java
@@ -0,0 +1,13 @@
+package com.profilematcherapp.profilematcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilematcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilematcherApplication.class, args);
+	}
+
+}
---
+++ profilematcher/src/test/java/com/profilematcherapp/profilematcher/ProfilematcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profilematcherapp.profilematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilematcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
