3a1ef2c1eaee80aca09b5468a8459f801e8791c2
csgtdev04 <62195113+csgtdev04@users.noreply.github.com>
2021-10-30 15:49:30 -0500

    entity 1 & pom updated

---
+++ profilematcher/src/main/java/com/profilematcherapp/profilematcher/Person.java
@@ -0,0 +1,25 @@
+package com.profilematcherapp.profilematcher;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "students_table")
+public class Person {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String description;
+    private String status;
+
+    @Column(name = "username")
+    private String username;
+}
